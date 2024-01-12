ae217ed7a96137d0bb4191b13668c4f502aa5860
IvanHuancaY <ivanyujra275@gmail.com>
2024-01-12 11:47:54 -0400

    Un readme

--- semana2/mi-primera-app/src/App.js
+++ semana2/mi-primera-app/src/App.js
@@ -14,7 +14,6 @@ function App() {
     Aprende a trabajar en equipo resolviendo increibles acertijos
     Animate a ser"
     />
-    <EventoB />
     </div>
   );
 }
--- semana2/mi-primera-app/src/components/EventoB.js
+++ semana2/mi-primera-app/src/components/EventoB.js
@@ -5,7 +5,7 @@ function EventoB(props){
     return(
         <div className="card-container">
             <img className="card-image"
-            src={require( '../images/${props.image}.jpeg')} 
+            src={require( '../images/evento1.jpeg')} 
             alt="Foto del evento" />
             <div className="card-content">
                 <h2 className="card-title">{props.title} </h2>
