e64304573e378eac0bd0043c78f66f76b9bb70a9
IvanHuancaY <ivanyujra275@gmail.com>
2024-01-23 09:49:33 -0400

    23-01-2024

---
+++ semana4/my-app/src/app/ejemplo/page.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Contador() {
+    return <h1>Contador</h1>
+    
+}
+
+export default Contador;
---
+++ semana4/my-app/src/app/page.js
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return <h1>Mi primera App de Next.js</h1>
+}
